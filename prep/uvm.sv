4d4add5f34ddb00080ffc6f7b0d3b2aaf0fb53c1
druvesque <dhruvsachdeva6480@gmail.com>
2026-01-18 14:09:29 +0530

    uvm

--- prep/uvm.sv
+++ prep/uvm.sv
@@ -1,4 +1,1 @@
-// Q1. Is UVM independent of SystemVerilog
-//
-//     - No, UVM is built on SystemVerilog and hence you cannot
-//       run UVM with any tool that does not support SystemVerilog.
+// Q1. 
