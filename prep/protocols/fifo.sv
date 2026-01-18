cecd979e7c0c9fd073aa44052e98f08dc70e0c8b
druvesque <dhruvsachdeva6480@gmail.com>
2026-01-19 01:38:21 +0530

    fifo

---
+++ prep/protocols/fifo.sv
@@ -0,0 +1,7 @@
+// Q. Explain FIFO and what different types of FIFOs are there?
+//
+//    - FIFO is a buffer that stores data such that the first data written
+//      is the first data read.
+//
+//    - Synchronous FIFO
+//      Same clock for read and write.
