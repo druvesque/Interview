b7c4768f7c25187dce1984335062a182619481c9
druvesque <dhruvsachdeva6480@gmail.com>
2026-01-21 23:12:51 +0530

    qa

---
+++ siemens_eda/4_dec.sv
@@ -0,0 +1,11 @@
+// Q. Design a sequence detector for 101011 (overlapping)?
+//
+// Q. Write an assertion in SystemVerilog such that if a then 
+//    b should be high for the next 2 clock cycles.
+//
+// Q. Describe different types of arrays in SystemVerilog.
+//
+// Q. What is your approach to ensure quality in a product? 
+//
+// Q. Explain always block in verilog and always_comb, always_ff
+//    & always_latch in SystemVerilog with the help of code.
